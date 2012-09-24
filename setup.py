81f2034dfbd590dbda233f8c020e26778df6bafb
Steffen Opel <sopel@opelbrothers.net>
2012-09-24 16:59:41 +0200

    Added LICENSE to setup.py.
    [Issue #2]

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import find_packages
 setup(name='botocross',
       version='1.0',
       packages=find_packages(),
+      license='LICENSE',
       install_requires=[
         "boto >= 2.5.2",
       ],
