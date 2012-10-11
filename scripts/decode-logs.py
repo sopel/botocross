c051dba5e992f3e19286036911103161f43c115d
Steffen Opel <sopel@opelbrothers.net>
2012-10-12 01:49:11 +0200

    Improved shebang compatibility.
    [Issue(s) #12]

--- scripts/decode-logs.py
+++ scripts/decode-logs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright (c) 2012 David Laing http://davidlaing.com/
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
--- scripts/delete-images.py
+++ scripts/delete-images.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright (c) 2012 Steffen Opel http://opelbrothers.net/
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
