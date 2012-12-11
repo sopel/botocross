49d7b6ca9b333b8e455fa3a845fea6d0090759e9
Steffen Opel <sopel@opelbrothers.net>
2012-12-11 23:49:28 +0100

    Bumped version to 1.1.0.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info <= (2, 4):
     sys.exit(1)
 
 setup(name="botocross",
-      version="1.0.9",
+      version="1.1.0",
       author="Steffen Opel",
       packages=find_packages(),
       scripts=[
